97d80754c9a58f4d8b8021e06af9332aa471863b
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 12:12:29 +0800

    Get the initial state and store in initialState variable

--- client/src/features/notes/notesSlice.jsx
+++ client/src/features/notes/notesSlice.jsx
@@ -1,5 +1,14 @@
 import { createEntityAdapter } from "@reduxjs/toolkit";
 
 // Adapter to normalize data
-// Stores data inside an object containing the an array if ids and the entities
+// Stores data inside an object containing an array if ids and the entities object
 const notesAdapter = createEntityAdapter({});
+
+// Set notes initial state
+// Initially the state is in the form of
+// {
+//     ids: [],         empty array
+//     entities: {}     empty object
+// }
+
+const initialState = notesAdapter.getInitialState();
