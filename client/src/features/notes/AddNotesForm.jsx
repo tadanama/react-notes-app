e13b485d20d14fe239a8d0aec3fa256b80a33f63
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 14:26:22 +0800

    Make react track the title and content state in addNotesForm

--- client/src/features/notes/AddNotesForm.jsx
+++ client/src/features/notes/AddNotesForm.jsx
@@ -1,15 +1,47 @@
 import React, { useState } from "react";
 
 function AddNotesForm() {
+	// Let react track the state of the form below
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+
+	function handleTitleChange(event) {
+		// Get the value of the title input text
+		const { value } = event.target;
+
+		// Set the title state to the input text value
+		setTitle(value);
+	}
+
+	function handleContentChange(event) {
+		// Get the value of the content textarea
+		const { value } = event.target;
+
+		// Set the content state to the textarea value
+		setContent(value);
+	}
+
 	return (
 		<>
 			<div className="form">
 				<h2>New note</h2>
 				<form>
 					<label htmlFor="title">Title:</label>
-					<input type="text" name="title" id="title" />
+					<input
+						type="text"
+						name="title"
+						id="title"
+						value={title}
+						onChange={handleTitleChange}
+					/>
 					<label htmlFor="content">Content:</label>
-					<textarea id="content" rows={"10"}/>
+					<textarea
+						name="content"
+						id="content"
+						rows={"10"}
+						value={content}
+						onChange={handleContentChange}
+					/>
 					<button>Create</button>
 				</form>
 			</div>
