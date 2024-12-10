575306c8d20eaa4f34c730bb73ceee01fe947b7b
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 14:52:55 +0800

    Create an async thunk to fetch the notes from api

--- client/src/features/notes/notesSlice.js
+++ client/src/features/notes/notesSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 // Define notes initial state
 const initiaState = {
@@ -7,10 +7,19 @@ const initiaState = {
 	error: null,
 };
 
+const POST_URL = "http://localhost:3500/notes";
+
+// Fetch notes from API
+// Can be done by using async thunk
+export const fetchNotes = createAsyncThunk("notes/fetchNotes", async () => {
+	// Send a request to get all notes
+	const response = await axios.get(POST_URL);
+	// Return the data from the response that we got to the fulfilled reducers
+	return response.data;
+});
+
 // Define the notes slice
 const notesSlice = createSlice({
-    name: "note",
-    reducers: {
-
-    }
-})
+	name: "note",
+	reducers: {},
+});
