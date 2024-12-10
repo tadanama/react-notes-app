ce21f15fcee40df470cbca506cc0e19d290cef19
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 20:47:33 +0800

    Dispatch fetchNotes async thunk as soon as the application is rendered

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
+import { fetchNotes } from "./features/notes/notesSlice.js";
 import "./index.css";
 import App from "./App.jsx";
+
+store.dispatch(fetchNotes());
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
