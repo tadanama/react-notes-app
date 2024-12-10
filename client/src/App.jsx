48b5f6f64e5a80199b2d739495ceedb5089f1407
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 14:16:21 +0800

    Render AddNotesForm below the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./component/Header";
+import AddNotesForm from "./features/notes/AddNotesForm";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<AddNotesForm />
 		</>
 	);
 }
