bc93cc2b9358e8c50b8dcf093afaa47753910917
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-11 07:47:12 +0800

    Render App component as the homepage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { store } from "./app/store.js";
 import { fetchNotes } from "./features/notes/notesSlice.js";
 import "./index.css";
@@ -11,7 +12,11 @@ store.dispatch(fetchNotes());
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</BrowserRouter>
 		</Provider>
 	</StrictMode>
 );
