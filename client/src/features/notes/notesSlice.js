b5aeeac1e5763278c3c73669fb05cd65fc7d2f5c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 12:29:15 +0800

    Retrieve the notes from the API and store in the notesAdapter

--- client/src/features/notes/notesSlice.js
+++ client/src/features/notes/notesSlice.js
@@ -1,4 +1,5 @@
 import { createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "../../api/apiSlice";
 
 // Adapter to normalize data
 // Stores data inside an object containing an array if ids and the entities object
@@ -10,5 +11,28 @@ const notesAdapter = createEntityAdapter({});
 //     ids: [],         empty array
 //     entities: {}     empty object
 // }
-
 const initialState = notesAdapter.getInitialState();
+
+// Define the logic when managing notes state
+const notesSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		// Get the notes
+		getNotes: builder.query({
+			// Send a get request to the <baseURL>/notes
+			query: "/notes",
+			// Transform the result from the query to update the initialState
+			transformResponse: (responseData) => {
+				return notesAdapter.setAll(initialState, responseData);
+			},
+			// Provide the tag to invalidate cache later on
+			providesTags: (result) => [
+				{ type: "Notes", id: "LIST" },
+				...result.ids.map((id) => {
+					type: "Notes", id;
+				}),
+			],
+		}),
+	}),
+});
+
+export const { useGetNotesQuery } = notesSlice;
