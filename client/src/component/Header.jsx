77cb7406f3964e29f928970dbd4db1ddcc3cb6fb
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 10:34:01 +0800

    Created Header component

---
+++ client/src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div className="header">
+			<h1>Notes</h1>
+			<nav>
+				<ul>
+					<li>My notes</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
+
+export default Header;
