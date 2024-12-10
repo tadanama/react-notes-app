eeaf5ee54b3d43ee09499807e4310690ff2388bc
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 13:36:46 +0800

    Start over

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,8 @@
-import { Routes, Route } from "react-router-dom";
 
-import Header from "./component/Header";
-import Layout from "./component/Layout";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Layout />}>
-			
-			</Route>
-		</Routes>
+		<h1>hi</h1>
 	);
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store.js";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/*" element={<App />} />
-				</Routes>
-			</BrowserRouter>
-		</Provider>
+		<App />
 	</StrictMode>
 );
