16e154cfd43fb60a8610af534845ccf9780e1ce3
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-10 21:59:46 +0800

    Render the NotesList component inside App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./component/Header";
 import AddNotesForm from "./features/notes/AddNotesForm";
+import NotesList from "./features/notes/NotesList";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<AddNotesForm />
+			<NotesList />
 		</>
 	);
 }
