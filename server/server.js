47a1f07d10848033b5d1704406912bd4019b1652
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-11 19:41:26 +0800

    Use notesRoutes to handle request to /notes endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, ()=>console.log(`Listening on port ${port}`))
+// Handles all request made to /notes endpoint
+app.use("/notes", notesRoutes);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
