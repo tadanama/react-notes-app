51c24d05c0aa086b6add69cce0886effdc969b8b
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-11 08:45:12 +0800

    Create the nested routes in App component
    - Render different component depending on the path(URL)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import Header from "./component/Header";
+import { Routes, Route } from "react-router-dom";
+
 import AddNotesForm from "./features/notes/AddNotesForm";
 import NotesList from "./features/notes/NotesList";
+import Layout from "./component/Layout";
+import SingleNotePage from "./features/notes/SingleNotePage";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<AddNotesForm />
-			<NotesList />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<NotesList />} />
+
+					<Route path="/note">
+						<Route index element={<AddNotesForm />} />
+						<Route path=":noteId" element={<SingleNotePage />} />
+						<Route path="edit/:noteId" element={<SingleNotePage />} />
+					</Route>
+				</Route>
+			</Routes>
 		</>
 	);
 }
