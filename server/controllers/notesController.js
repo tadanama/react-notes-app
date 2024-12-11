330b761cc5b4faaaf087eb32475035cb6deecf6c
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-11 23:15:54 +0800

    Created controller to update an existing note

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -19,19 +19,58 @@ export const addNewNote = async (req, res) => {
 	// Get the note title and text
 	const { title, text } = req.body;
 
-    // Generate new id
+	// Generate new id
 	const newNoteId = uuidv4();
 
-    // Send POST request
-    //TODO remove the hard coded id when authentication is implemented
+	// Send POST request
+	//TODO remove the hard coded id when authentication is implemented
 	try {
 		const result = await pool.query(
 			"INSERT INTO notes (note_id, note_title, note_text, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
 			[newNoteId, title, text, "1"]
 		);
 
-        // Send the newly created note
-        res.json(result.rows[0]);
+		// Send the newly created note
+		res.json(result.rows[0]);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// Update the note
+export const updateNote = async (req, res) => {
+	// Get the note id from req.params
+	const { noteId } = req.params;
+
+	// Get the note info to update
+	const { title, text } = req.body;
+
+	// Check if the notes that we are trying to update exists
+	try {
+		const foundNote = await pool.query(
+			"SELECT * from notes WHERE note_id = $1",
+			[noteId]
+		);
+
+		// If there are no rows returned means that the post do not exist
+		// Return 204 status code
+		if (!foundNote.rows) {
+			res.status(400);
+			return res.json("Note do not exist");
+		}
+
+		// Update the notes to the database
+		try {
+			const result = await pool.query(
+				"UPDATE notes SET note_title = $1, note_text = $2, created_date = CURRENT_DATE WHERE note_id = $3 RETURNING *",
+				[title, text, noteId]
+			);
+			res.json(result.rows[0]);
+		} catch (error) {
+			console.log(error);
+		}
+
+		// Check if user provide title or not
 	} catch (error) {
 		console.log(error);
 	}
