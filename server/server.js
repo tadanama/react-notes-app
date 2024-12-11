617789ca7c84541e1c4af2cf5b55bfaaad4b465a
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-11 22:19:53 +0800

    Parse the incoming JSON data and attach it to the req.body

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import notesRoutes from "./routes/notesRoutes.js";
 const app = express();
 const port = 3000;
 
+// Enable express to parse JSON data and attach it to req.body
+app.use(express.json());
+
 // Handles all request made to /notes endpoint
 app.use("/notes", notesRoutes);
 
