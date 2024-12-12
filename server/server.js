847b3abef3d84355c86a6c4a6b637dddf468946e
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-12 08:18:11 +0800

    Set cors middleware
    - Allow frontend to make request to the server from different domain

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 // Enable express to parse JSON data and attach it to req.body
 app.use(express.json());
+
+// Allow request from frontend
+app.use(cors());
 
 // Handles all request made to /notes endpoint
 app.use("/notes", notesRoutes);
