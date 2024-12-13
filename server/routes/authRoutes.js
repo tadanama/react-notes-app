6ab3161dbb96436d50cda7680390eb9029b03aa2
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-13 17:04:02 +0800

    Create an endpoint to refresh the access token

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,6 +8,9 @@ router.post("/login", (req, res) => {});
 // Route to register new user
 router.post("/register", (req, res) => {});
 
+// Route to generate refresh the access token
+router.get("/refresh", (req, res) => {});
+
 // Route when user logout
 router.post("/logout", (req, res) => {});
 
