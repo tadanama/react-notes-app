304c0172456f5799097d5c42ff613940c2f55b79
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-13 10:46:25 +0800

    Manage login input state with useState

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+	// Track email and password state
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	// Handle email input change
+	function handleEmailChange(event) {
+		const { value } = event.target;
+
+		setEmail(value);
+	}
+
+	// Handle password input change
+	function handlePasswordChange(event) {
+		const { value } = event.target;
+
+		setPassword(value);
+	}
+
 	return (
 		<>
 			<h1 className="auth-h1">Login</h1>
 			<form className="auth-form">
 				<label htmlFor="email">Email address</label>
-				<input type="email" name="email" id="email" />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					value={email}
+					onChange={handleEmailChange}
+				/>
 
 				<br />
 
 				<label htmlFor="password">Password</label>
-				<input type="password" name="password" id="password" />
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					onChange={handlePasswordChange}
+				/>
 
 				<br />
 
