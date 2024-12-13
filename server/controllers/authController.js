2ddd98e8d246c7245b7da88e014e41e7931c407f
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-13 17:06:41 +0800

    Create auth controller file that handles the logic of user authentication

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+// Login controller
+export function login (req, res) {}
+
+// Signup controller
+export function signup(req, res){}
+
+// Refresh controller
+export function refresh(req, res){}
+
+//  Logout controller
+export function logout(req, res){}
