b1af9727bd04ff097546502585ac2b444fc09922
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-14 08:22:57 +0800

    Add the auth controllers to the auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,18 @@
 import express from "express";
+import { login, signup, refresh, logout } from "../controllers/authController";
 
 const router = express.Router();
 
 // Route for user to login
-router.post("/login", (req, res) => {});
+router.post("/login", login);
 
 // Route to register new user
-router.post("/register", (req, res) => {});
+router.post("/register", signup);
 
 // Route to generate refresh the access token
-router.get("/refresh", (req, res) => {});
+router.get("/refresh", refresh);
 
 // Route when user logout
-router.post("/logout", (req, res) => {});
+router.post("/logout", logout);
 
 export default router;
