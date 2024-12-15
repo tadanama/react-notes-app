f7e4d0af768a60ebf4bf900da11837bd4c5a9fdc
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-15 17:20:27 +0800

    Remove the single note component and route
    The EditNoteForm is used to display the single note

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import AddNotesForm from "./features/notes/AddNotesForm";
 import NotesList from "./features/notes/NotesList";
 import Layout from "./component/Layout";
-import SingleNotePage from "./features/notes/SingleNotePage";
 import EditNoteForm from "./features/notes/EditNoteForm";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
 
 					<Route path="/note">
 						<Route index element={<AddNotesForm />} />
-						<Route path=":noteId" element={<SingleNotePage />} />
 						<Route path="edit/:noteId" element={<EditNoteForm />} />
 					</Route>
 				</Route>
